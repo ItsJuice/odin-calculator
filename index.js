a5ecbd7fda85f882f6a4a06732f5d440f9be7428
Oli Skertchly <oli@itsjuice.com>
2023-01-12 15:08:35 +0000

    Create screen and working number and operator buttons

--- index.js
+++ index.js
@@ -15,20 +15,88 @@ function divide(a, b){
 };
 
 function operate(a, operator, b){
-    switch (operator) {
-        case "plus":
-            return add(a, b)
-        case "minus":
-            return subtract(a, b)
-        case "times":
-            return multiply(a, b)
-        case "divide by":
-            return divide(a, b)
-    };
+  switch (operator) {
+    case "plus":
+      return add(a, b)
+    case "minus":
+      return subtract(a, b)
+    case "times":
+      return multiply(a, b)
+    case "divide by":
+      return divide(a, b)
+  };
 };
 
-let answer = operate (10, "divide by", 7).toFixed(2);
+// let answer = operate (10, "divide by", 3);
 
-answer = +answer;
+// answer = Math.round (answer * 1e2) / 1e2;
 
-console.log(answer);
+// console.log(answer);
+
+var inputArray = [];
+var inputString = "";
+var calcString = "";
+
+function clearScreen() {
+  //clear the input array
+
+  //clear the screens
+  const outputDiv = document.getElementById("outputScreen");
+  outputDiv.textContent = "0";
+  const calculationDiv = document.getElementById("calculationScreen");  
+  calculationDiv.textContent  = "";
+  inputString = "";
+
+}
+
+function backSpace() {
+  //remove the last number
+}
+
+  
+
+function numberButton(number){
+  // clear 0 if it is the first element in the input string 
+  // console.log(inputString.charAt(0));
+  
+  if (inputString === "0") inputString = "";
+
+
+  //append the input number to the input string
+  inputString += number;
+
+  //send the input string to the input screen
+  const outputDiv = document.getElementById("outputScreen");
+  outputDiv.textContent  = inputString;
+
+
+  //send the input number to the calculation screen
+  const calculationDiv = document.getElementById("calculationScreen");  
+  calculationDiv.textContent += number;
+  
+}
+
+function operatorButton(operator){
+  //check that the last element in the inputArray is a number and not a dot or another operator
+  
+  //clear the input screen
+  const outputDiv = document.getElementById("outputScreen");
+  outputDiv.textContent = "";
+
+  //clear the input string
+  inputString = "";
+  
+  //add the operator string to the 
+  // const calcString = operator;
+  const calculationDiv = document.getElementById("calculationScreen");
+  calculationDiv.textContent += operator;
+}
+
+function dotButton(){
+
+}
+
+function equalsButton(){
+
+}
+
